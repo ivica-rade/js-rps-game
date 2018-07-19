820c767c07dcdf7eaaad2e2df2f2ab0e565601e6
Ivica Rade <ivica.rade3@gmail.com>
2018-07-19 22:44:52 +0200

    some logic

---
+++ app.js
@@ -0,0 +1,17 @@
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+
+var computerChoice = Math.random();
+
+var playerChoice = 
+
+if(computerChoice < 0.34){
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67){
+    computerChoice = "paper";
+} else{
+    computerChoice = "scissors";
+}
+
+
