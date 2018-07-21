750246c9e6b582e9cd2b47f03c6f78e15281bf12
Ivica Rade <ivica.rade3@gmail.com>
2018-07-21 21:46:36 +0200

    added game reset function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
+var restart = document.getElementById("restart");
+
+//simplest game reload option there is
+restart.addEventListener("click", function(){
+    location.reload();
+});
+
+//hides and unhides the Play Again button
+function resetGame(){
+    if(restart.style.display === "none"){
+        restart.style.display = "block";
+    } else{
+        restart.style.display = "none";
+    }
+};
 
 // Calculates computer's choice randomly
 var computerChoice = Math.random();
@@ -16,18 +31,21 @@ if(computerChoice < 0.34){
 rock.addEventListener("click", function(){
     playerChoice = "rock";
     getWinner(computerChoice, playerChoice);
+    resetGame();
 });
 
 //if a player chooses paper
 paper.addEventListener("click", function(){
     playerChoice = "paper";
     getWinner(computerChoice, playerChoice);
+    resetGame();
 });
 
 //if a player chooses scissors
 scissors.addEventListener("click", function(){
     playerChoice = "scissors";
     getWinner(computerChoice, playerChoice);
+    resetGame();
 });
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-A simple rock, paper, scissors game made in JavaScript
+<h1>A simple rock, paper, scissors game made in JavaScript</h1>
 
 Disgusting desing, but that's not the point. I may re-do it later on, wanted to practice logic. 
 
-todo: add a "Play again" function
